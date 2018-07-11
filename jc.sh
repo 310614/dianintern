62dcba8a520e058e3535df874aeb19720d4c9034
fjn <569831010@qq.com>
2018-07-11 18:36:20 +0800

    在shell中添加了当没有输入参数时的usage部分

--- jc.sh
+++ jc.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-n=$1
-fac=1
-while [ $n -gt 0 ]
-do
-fac=$((fac * n))
-n=$((n - 1))
-done
-echo $fac
+#先判断是否有数字输入
+if [ -n "$1" ];then
+#若有数字输入，则进行阶乘计算
+   n=$1
+   fac=1
+   while [ $n -gt 0 ]
+   do
+      fac=$((fac * n))
+      n=$((n - 1))
+   done
+   echo $fac
+#若没有数字输入，则按题中要求echo出下列语句
+else
+   echo "usage: jc.sh [n]"
+   echo "calculates a number's factorial"
+fi
